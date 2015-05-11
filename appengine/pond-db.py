b02c151deab42bbabb96dc141d1588a70bbdcd81
Quynh Neutron <qneutron@gmail.com>
2015-05-10 19:51:53 -0700

    Starting database
    Defined Player and Duck entities

--- appengine/pond-db.py
+++ appengine/pond-db.py
@@ -19,10 +19,48 @@ limitations under the License.
 """Redirect to user's page.
 """
 
-__author__ = "fraser@google.com (Neil Fraser)"
+__author__ = "neutron@google.com (Quynh Neutron)"
 
 from google.appengine.api import users
+from google.appengine.ext import ndb
 
+
+class Player(ndb.Model):
+    id = ndb.KeyProperty(kind="int")
+    user_id = ndb.IntegerProperty(required=True) #must be unique
+    name = ndb.StringProperty(indexed=False)
+    description = ndb.TextProperty()
+    deleted = ndb.BooleanProperty(default=False)
+
+class Duck(ndb.Model):
+    id = ndb.KeyProperty(kind="int")
+    player_id = ndb.IntegerProperty()
+    player_name = ndb.StringProperty(indexed=False)
+    level = ndb.IntegerProperty()
+    name = ndb.StringProperty(indexed=False)
+    description = ndb.TextProperty()
+    program_xml = ndb.TextProperty()
+    program_js = ndb.TextProperty()
+    velocity = ndb.IntegerProperty()
+    created = ndb.DateTimeProperty(auto_now_add=True)
+    modified = ndb.DateTimeProperty(auto_now=True)
+    deleted = ndb.BooleanProperty(default=False)
+
+    @classmethod
+    def ducks_by_owner(cls, player_key):
+        return cls.query(player_id=player_key).order(-cls.modified)
+
+    @classmethod
+    def ducks_by_level(cls, level):
+        return cls.query(level=level).order(-cls.modified)
+
+# Check if user exist, then maybe create user if not
 user = users.get_current_user()
 print("Status: 302")
-print("Location: /pond-user?id=%s" % user.user_id())
+print("Location: /pond-player?id=%s" % user.user_id)
+
+#player_id = Player.query(Player.user_id == user.user_id).get()
+#Can't use Player.get_by_id(user.user_id) because user_id is not id
+
+# Are all KeyProperties auto-filled?
+# How to ensure some property is unique?
