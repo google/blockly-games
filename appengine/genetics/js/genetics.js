53abfb59ad511233e2dd94c66b05aaf908285e5a
kozbial <kozbial@google.com>
2016-06-01 17:15:27 -0700

    Adding blocks to toolbox and default code
    Adding blocks to toolbox and adding colors. Creating defualt code that makes the 3 main game functions (to be later changed to custom blocks).

--- appengine/genetics/js/genetics.js
+++ appengine/genetics/js/genetics.js
@@ -132,10 +132,66 @@ Genetics.init = function() {
         'trashcan': true,
         'zoom': {'controls': true, 'wheel': true}
       });
-  Blockly.JavaScript.addReservedWords('me');
+  Blockly.JavaScript.addReservedWords('');
   var defaultXml =
-      '<xml>' +
-      '</xml>';
+  '<xml>' +
+    '<block type="procedures_defreturn" deletable="false" editable="false" x="0" y="150">' +
+      '<mutation>' +
+        '<arg name="me"></arg>' +
+        '<arg name="aliveMice"></arg>' +
+      '</mutation>' +
+      '<field name="NAME">pickFight</field>' +
+      '<comment pinned="false" h="86" w="631">' +
+'Chooses and returns mouse from aliveMice to pick a fight with or null to choose\n' +
+'no mouse and never fight again.\n' +
+'Choosing to fight against itself will kill the mouse.\n' +
+'@param me the mouse picking a fight\n' +
+'@param aliveMice all mice currently in cage\n' +
+'@returns the mouse chosen to attempt to mate with or null if no mouse is chosen' +
+      '</comment>' +
+      '<value name="RETURN">' +
+        '<shadow type="logic_null"></shadow>' +
+      '</value>' +
+    '</block>' +
+    '<block type="procedures_defreturn" deletable="false" editable="false" x="0" y="350">' +
+      '<mutation>' +
+        '<arg name="me"></arg>' +
+        '<arg name="aliveMice"></arg>' +
+      '</mutation>' +
+      '<field name="NAME">chooseMate</field>' +
+      '<comment pinned="false" h="86" w="631">' +
+'Chooses and returns mouse from aliveMice to attempt to mate with or null to\n' +
+'choose no mouse and never mate again. If the mate chosen is valid and agrees to\n' +
+'the request then a child will be born.\n' +
+'@param me the mouse choosing a mate\n' +
+'@param aliveMice all mice currently in cage\n' +
+'@returns the mouse chosen to attempt to mate with or null if no mouse is chosen' +
+      '</comment>' +
+      '<value name="RETURN">' +
+        '<shadow type="logic_null"></shadow>' +
+      '</value>' +
+    '</block>' +
+    '<block type="procedures_defreturn" deletable="false" editable="false" x="0" y="550">' +
+      '<mutation>' +
+        '<arg name="suitor"></arg>' +
+        '<arg name="me"></arg>' +
+        '<arg name="aliveMice"></arg>' +
+      '</mutation>' +
+      '<field name="NAME">mateAnswer</field>' +
+      '<comment pinned="false" h="86" w="631">' +
+'Returns true to agree to mate or false to decline.\n' +
+'@param suitor the mouse requesting to mate\n' +
+'@param me the mouse choosing a mate\n' +
+'@param aliveMice all mice currently in cage\n' +
+'@returns the the answer to the mating request' +
+      '</comment>' +
+      '<value name="RETURN">' +
+        '<shadow type="logic_boolean">' +
+          '<field name="BOOL">TRUE</field>' +
+        '</shadow>' +
+      '</value>' +
+    '</block>' +
+  '</xml>';
   var xml = Blockly.Xml.textToDom(defaultXml);
   // Clear the workspace to avoid merge.
   BlocklyGames.workspace.clear();
