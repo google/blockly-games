78cd1a7c88f0fd2d5a35f481db37e964c223493e
Neil Fraser <fraser@google.com>
2019-08-23 13:58:55 -0700

    Remove goog.ui.AnimatedZippy from Pond docs
    Old: 98 dependencies, 67kb
    New: 55 dependendencies, 43kb

--- appengine/pond/docs/js/docs.js
+++ appengine/pond/docs/js/docs.js
@@ -25,9 +25,8 @@
 
 goog.provide('Pond.Docs');
 
+goog.require('BlocklyGames');
 goog.require('Pond.Docs.soy');
-
-goog.require('goog.ui.AnimatedZippy');
 
 
 /**
@@ -44,8 +43,29 @@ Pond.Docs.init = function() {
   // Turn all h2 tags into zippies.
   var headers = document.getElementsByTagName('h2');
   for (var i = 0, header; (header = headers[i]); i++) {
-    new goog.ui.AnimatedZippy(header, header.id + '-content', false);
+    var img = document.createElement('img');
+    img.src = '../common/1x1.gif';
+    header.insertBefore(img, header.firstChild);
+    header.className = 'zippy-header-collapsed';
+    var content = document.getElementById(header.id + '-content');
+    content.className = 'zippy-content-collapsed';
+    BlocklyGames.bindClick(header, Pond.Docs.toggle);
   }
+};
+
+/**
+ * Toggle one section open or closed.
+ * @param {!Event} e The click or touch event.
+ */
+Pond.Docs.toggle = function(e) {
+  var header = e.currentTarget;
+  var content = document.getElementById(header.id + '-content');
+  var isOpen = content.className == 'zippy-content-expanded';
+  header.className =
+      'zippy-header-' + (isOpen ? 'collapsed' : 'expanded');
+  content.className =
+      'zippy-content-' + (isOpen ? 'collapsed' : 'expanded');
+  content.style.maxHeight = isOpen ? 0 : (content.scrollHeight + 'px');
 };
 
 window.addEventListener('load', Pond.Docs.init);
