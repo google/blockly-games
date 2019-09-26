4ad3baabe864e4b1688cb55ee1ba14edd539c74a
Neil Fraser <fraser@google.com>
2019-09-26 14:33:40 -0700

    Update build script

--- build.py
+++ build.py
@@ -16,24 +16,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os, shutil
+import os
+import os.path
+import shutil
 
 MASTER_DIR = "../blockly-games/"
 APPENGINE_DIR = MASTER_DIR + "appengine/"
-JSON_DIR = MASTER_DIR + "json/"
 GENERATED_DIR = "generated/"
 CWD = os.getcwd()
 
 # Check for all required directories up front.
-for directory in (MASTER_DIR, JSON_DIR, APPENGINE_DIR):
+for directory in (MASTER_DIR, APPENGINE_DIR):
   if not os.path.isdir(directory):
     raise IOError("Can't find blockly-games source directory:\n" + directory)
 
 # Find all required languages.
 languages = []
-for filename in os.listdir(JSON_DIR):
-  if filename.endswith(".json") and filename not in ("qqq.json", "keys.json"):
-    languages.append(filename[:-5])
+for directory in os.listdir(MASTER_DIR + "appengine/index/generated/"):
+  if os.path.isfile(MASTER_DIR + "appengine/index/generated/" + directory + "/compressed.js"):
+    languages.append(directory)
 if len(languages) == 0:
   raise IndexError("No languages found.")
 languages.sort()
@@ -53,18 +54,14 @@ for language in languages:
 
   # Create index.html redirect file.
   f = open(GENERATED_DIR + "blockly-games/index.html", "w")
-  f.write("""
-<html>
-  <head>
-    <meta http-equiv=refresh content='0; url=%s/index.html' />
-  </head>
-</html>""" % language)
+  f.write("""<html><head>
+<meta http-equiv=refresh content="0; url=%s/index.html"/>
+</head></html>""" % language)
   f.close()
 
   # Copy the file tree.
   directory = GENERATED_DIR + "blockly-games/" + language + "/"
   shutil.copytree(APPENGINE_DIR, directory, ignore=ignore)
-  shutil.rmtree(directory + "accessible")
   shutil.rmtree(directory + "gallery")
   shutil.rmtree(directory + "gallery_api")
   shutil.rmtree(directory + "genetics")
@@ -81,21 +78,14 @@ for language in languages:
   shutil.move(GENERATED_DIR + "blockly-games/media",
               directory + "third-party/blockly/media/")
 
-  shutil.rmtree(directory + "third-party/goog/")
-  shutil.rmtree(directory + "third-party/third_party_goog/")
-  shutil.rmtree(directory + "third-party/midi-js/")
+  shutil.rmtree(directory + "generated/")
   shutil.rmtree(directory + "third-party/JS-Interpreter/demos/")
   for filename in os.listdir(directory + "third-party/JS-Interpreter/"):
-    if filename != "compiled.js":
+    if filename != "compressed.js":
       os.remove(directory + "third-party/JS-Interpreter/" + filename)
-  for dirname in os.listdir(directory + "third-party/midi-js-soundfonts/"):
-    for filename in os.listdir(directory + "third-party/midi-js-soundfonts/"
-                               + dirname + "/"):
-      if filename not in ("C3.mp3", "D3.mp3", "E3.mp3", "F3.mp3", "G3.mp3",
-                          "A3.mp3", "B3.mp3", "C4.mp3", "D4.mp3", "E4.mp3",
-                          "F4.mp3", "G4.mp3", "A4.mp3"):
-        os.remove(directory + "third-party/midi-js-soundfonts/"
-                  + dirname + "/" + filename)
+  for dirname in os.listdir(directory + "third-party/soundfonts/"):
+    if os.path.isdir("third-party/soundfonts/" + dirname):
+      os.remove(directory + "third-party/soundfonts/" + dirname + "/B4.mp3")
   for filename in os.listdir(directory + "third-party/SoundJS/"):
     if filename != "soundjs.min.js":
       os.remove(directory + "third-party/SoundJS/" + filename)
@@ -106,7 +96,12 @@ for language in languages:
         if langname != language:
           shutil.rmtree(subdirectory + "/" + langname)
   for filename in ('admin.html', 'gallery.html', 'robots.txt', 'genetics.html',
-                   'common/debug.js', 'common/stripes.gif', 'common/storage.js'):
+      'common/debug.js', 'common/stripes.gif', 'common/storage.js',
+      'third-party/base.js', 'third-party/soyutils.js',
+      'third-party/soundfonts/README.txt',
+      'third-party/blockly/media/pilcrow.png',
+      'third-party/blockly/media/sprites.svg',
+      'index/title.png', 'apple-touch-icon.png'):
     os.remove(directory + filename)
 
   # Create single-language bootloader.
