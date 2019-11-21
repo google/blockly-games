a328b9919a61fd3345e8201f66ba2ce449893f7c
Monica Kozbial <kozbial@google.com>
2019-11-21 13:58:46 -0800

    Updating model logic to use ancestor keys. (#141)

--- appengine/pond_storage/common.py
+++ appengine/pond_storage/common.py
@@ -28,13 +28,13 @@ class Code(ndb.Model):
   opt_xml = ndb.TextProperty()
 
 class Duck(ndb.Model):
-  userid = ndb.StringProperty()  # hashed google id
   name = ndb.StringProperty(indexed=False, required=True)
   code = ndb.LocalStructuredProperty(Code, indexed=False, required=True)
 
   @classmethod
   def _pre_delete_hook(cls, key):
-    le = key.get().leaderboard_entry
+    duck = key.get()
+    le = duck.leaderboard_entry
     if le:
       le.delete()
 
@@ -47,15 +47,17 @@ class LeaderboardEntry(ndb.Model):
 Duck.leaderboard_entry = ndb.KeyProperty(indexed=False, kind=LeaderboardEntry)
 Duck._fix_up_properties()
 
+def get_user_key(user):
+  return ndb.Key('User', user.user_id())
+
 """Returns list of current user's Ducks (including name and urlsafe duck id."""
 def get_user_ducks():
-  user = users.get_current_user()
-  userid = user.user_id()
-  duckQuery = Duck.query(Duck.userid == userid)
-  duckList = []
-  for duck in duckQuery:
-    duckList.append({'name': duck.name, 'duckUrl': duck.key.urlsafe()})
-  return duckList
+  user_key = get_user_key(users.get_current_user())
+  duck_query = Duck.query(ancestor=user_key)
+  duck_list = []
+  for duck in duck_query:
+    duck_list.append({'name': duck.name, 'duckUrl': duck.key.urlsafe()})
+  return duck_list
 
 """Verifies whether duck exists and is owned by current user."""
 def verify_duck(duck):
@@ -66,8 +68,7 @@ def verify_duck(duck):
     return False
   else:
     # Verify user is owner of duck
-    user = users.get_current_user()
-    if duck.userid != user.user_id():
+    if duck.key.parent() != get_user_key(users.get_current_user()):
       # User cannot delete this duck (user is not owner).
       print("Status: 401 Unauthorized")
       return False
@@ -82,17 +83,15 @@ def delete_duck(duck):
 
 """Creates duck with the given attributes and returns key."""
 def create_duck(name, code):
-  user = users.get_current_user()
-  userid = user.user_id()
-  # Verify user does not have too many Ducks
-  max_ducks = 10
-  owned_ducks_query = Duck.query(Duck.userid == userid)
-  owned_ducks_count = owned_ducks_query.count(limit=max_ducks)
-  if owned_ducks_count >= max_ducks:
+  user_key = get_user_key(users.get_current_user())
+  duck_query = Duck.query(ancestor=user_key)
+  duck_count = duck_query.count(limit=10)
+  if duck_count == 10:
     # There are too many ducks!!
     print("Status: 403 Owner has too many ducks")
+    return None
   else:
     # Create a new Duck entry
-    duck = Duck(userid=userid, name=name, code=code)
+    duck = Duck(name=name, code=code, parent=user_key)
     duck_key = duck.put()
     return duck_key
