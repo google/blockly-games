7407323b71cf04612a11c64d18d04862b22b405a
Abby <aschmiedt@google.com>
2019-12-11 08:49:32 -0800

    Update algorithm for getting opponent ducks

--- appengine/pond_storage/opponents.py
+++ appengine/pond_storage/opponents.py
@@ -19,20 +19,42 @@ limitations under the License.
 
 import cgi
 import json
+import math
+import random
 from google.appengine.ext import ndb
 from pond_storage import *
 
-def get_opponent_ducks(duck):
+def get_ducks_in_range(user_key, duck_ranking, duck_list, percent_range):
+  """Get ducks with ranks between percent above and below current duck rank"""
+  max_ranking = math.ceil(duck_ranking + len(duck_list) * percent_range)
+  min_ranking = math.floor(duck_ranking - len(duck_list) * percent_range)
+  opponent_ducks = []
+  for duck in duck_list:
+    if duck.leaderboard_entry_key and duck.key.parent() != user_key:
+      rank = duck.leaderboard_entry_key.get().ranking
+      if rank <= max_ranking and rank >= min_ranking:
+        opponent_ducks.append(get_duck_info(duck))
+  return opponent_ducks
+
+def get_opponent_ducks(user_duck):
   """Returns list of opponent ducks."""
-  # TODO: Add filter for published ducks
-  # TODO: Add logic here to pick the duck based on the ranking
   user_key = get_user_key(users.get_current_user())
-  duck_query = Duck.query()
-  duck_list = []
-  for duck in duck_query:
-    if duck.key.parent() != user_key:
-      duck_list.append(get_duck_info(duck))
-  return duck_list[0:3]
+  duck_ranking = user_duck.leaderboard_entry_key.get().ranking
+  duck_list = Duck.query(Duck.published == True).fetch()
+  opponent_ducks = []
+  percent_range = .10
+
+  # Increase range until the length of the opponent ducks is greater than 3
+  while (len(opponent_ducks) < 3) or percent_range > 1:
+    opponent_ducks = get_ducks_in_range(user_key, duck_ranking, duck_list, percent_range)
+    opponent_len = len(opponent_ducks)
+    if (opponent_len > 1):
+      percent_range = percent_range + .10
+    elif (opponent_len >= 0):
+      percent_range = percent_range + .20
+  random.shuffle(opponent_ducks)
+  return opponent_ducks[0:3]
+
 
 forms = cgi.FieldStorage()
 if forms.has_key('key'):
