af34e4948ba1b614ead30e2281a7c575963d74b3
Monica Kozbial <kozbial@google.com>
2020-01-31 13:25:40 -0800

    Adding create duck with default code functionality. (#185)
    * Adding create duck with default code functionality.
    
    * Adding default argument value.

--- appengine/pond/duck/online/js/online.js
+++ appengine/pond/duck/online/js/online.js
@@ -67,7 +67,7 @@ Pond.Duck.Online.init = function() {
       .style.visibility = 'hidden';
   Pond.Duck.tabContent[Pond.Duck.TAB_INDEX.JAVASCRIPT]
       .style.visibility = 'hidden';
-
+  // Load content or open dialog to choose content to load
   if (Pond.Duck.Online.duckKey) {
     Pond.Duck.Datastore.getDuck(
         Pond.Duck.Online.duckKey, Pond.Duck.Online.loadDuck,
@@ -81,7 +81,7 @@ Pond.Duck.Online.init = function() {
     Pond.reset();
     Pond.Duck.Online.openDuckList_();
   }
-
+  // Bind buttons
   BlocklyGames.bindClick('duckListButton', Pond.Duck.Online.openDuckList_);
   BlocklyGames.bindClick('duckCreateButton', Pond.Duck.Online.showCreateDuckForm);
   BlocklyGames.bindClick('getOpponents', function() {
@@ -269,159 +269,6 @@ Pond.Duck.Online.openDuckList_ = function() {
   };
   var origin = document.getElementById('duckListButton');
   BlocklyDialogs.showDialog(content, origin, true, true, style);
-};
-
-/* Logic for Buttons and Forms for debugging purposes. */
-// TODO: remove debugging forms as features continue to be implemented.
-/**
- * Store user code in specified form.
- * @param {string} formPrefix Prefix of element ids for form.
- */
-Pond.Duck.Online.storeUserCodeInForm_ = function(formPrefix) {
-  // Store the user code in the form inputs
-  document.getElementById(formPrefix +'Js').value =
-      BlocklyInterface.getJsCode();
-  document.getElementById(formPrefix + 'Xml').value =
-      BlocklyInterface.blocksDisabled ? '' : BlocklyInterface.getXml();
-};
-/**
- * Display dialog based on specified form prefix.
- * @param {string} formPrefix Prefix of element ids for form.
- * @param {string} defaultFocusElId Element id of element to focus after opening
- *    form.
- * @param {boolean} storeUserCode Whether to store user code in element.
- */
-Pond.Duck.Online.showDuckForm_ = function(formPrefix, defaultFocusElId, storeUserCode) {
-  if (storeUserCode) {
-    Pond.Duck.Online.storeUserCodeInForm_(formPrefix);
-  }
-  var content = document.getElementById(formPrefix + 'Dialog');
-  var style = {
-    width: '40%',
-    left: '30%',
-    top: '3em'
-  };
-  var origin = document.getElementById(formPrefix + 'Button');
-  BlocklyDialogs.showDialog(content, origin, true, true, style);
-  // Wait for the opening animation to complete, then focus the title field.
-  setTimeout(function() {
-    document.getElementById(defaultFocusElId).focus();
-  }, 250);
-};
-/**
- * Bind button click events for specified form.
- * @param {string} formPrefix Prefix of element ids for form.
- * @param {!Function} formSubmitCallback Callback for form submit.
- */
-Pond.Duck.Online.bindFormButtonEvents_ = function(formPrefix, formSubmitCallback) {
-  var cancel = document.getElementById(formPrefix + 'Cancel');
-  cancel.addEventListener('click', BlocklyDialogs.hideDialog, true);
-  cancel.addEventListener('touchend', BlocklyDialogs.hideDialog, true);
-  var ok = document.getElementById(formPrefix + 'Ok');
-  ok.addEventListener('click',formSubmitCallback, true);
-  ok.addEventListener('touchend', formSubmitCallback, true);
-};
-/**
- * Display a dialog for creating a duck.
- */
-Pond.Duck.Online.showCreateDuckForm = function() {
-  if (!Pond.Duck.Online.showCreateDuckForm.runOnce_) {
-    Pond.Duck.Online.bindFormButtonEvents_('duckCreate', Pond.Duck.Online.duckCreateForm);
-    // Only bind the buttons once.
-    Pond.Duck.Online.showCreateDuckForm.runOnce_ = true;
-  }
-  Pond.Duck.Online.showDuckForm_('duckCreate', 'duckCreateName', true);
-};
-/**
- * Display a dialog for updating a duck.
- */
-Pond.Duck.Online.showUpdateDuckForm = function() {
-  if (!Pond.Duck.Online.showUpdateDuckForm.runOnce_) {
-    Pond.Duck.Online.bindFormButtonEvents_('duckUpdate', Pond.Duck.Online.duckUpdateForm);
-    // Only bind the buttons once.
-    Pond.Duck.Online.showUpdateDuckForm.runOnce_ = true;
-  }
-  Pond.Duck.Online.showDuckForm_('duckUpdate', 'duckUpdateDuckKey', true);
-};
-/**
- * Display a dialog for deleting a duck.
- */
-Pond.Duck.Online.showDeleteDuckForm = function() {
-  if (!Pond.Duck.Online.showDeleteDuckForm.runOnce_) {
-    Pond.Duck.Online.bindFormButtonEvents_('duckDelete', Pond.Duck.Online.duckDeleteForm);
-    // Only bind the buttons once.
-    Pond.Duck.Online.showDeleteDuckForm.runOnce_ = true;
-  }
-  Pond.Duck.Online.showDuckForm_('duckDelete', 'duckDeleteDuckKey', false);
-};
-/**
- * Creates a popup with information on what action was completed, or an error message.
- * @param {string} action The action completed. (created, deleted, updated)
- */
-Pond.Duck.Online.createDuckFormOnLoadCallback_ = function(action) {
-  return function() {
-    var meta = JSON.parse(this.responseText);
-    var text = 'Duck ' + action + ' with key: ' + meta['duck_key'];
-    BlocklyDialogs.storageAlert(null, text);
-  };
-};
-/**
- * Encode form elements in map.
- * @param {HTMLFormElement} form Form to encode elements from.
- * @return {Array<string>} Encoded elements in a list of strings in the form:
- *    "<name>=<value>"
- * @private
- */
-Pond.Duck.Online.encodeFormElements_ = function(form) {
-  var data = [];
-  for (var i = 0, element; (element = form.elements[i]); i++) {
-    if (element.name) {
-      data.push(encodeURIComponent(element.name) + '='
-          + encodeURIComponent(element.value));
-    }
-  }
-  return data
-};
-/**
- * Submits the form information.
- * @param {Array.<string>} requiredFieldsIds The required ids for the fields we
- *     want to send in the request.
- * @param {string} formId The id of the form used to collect information on
- *     what info to send in the request.
- * @param {string} action The action completed. (created, deleted, updated)
- */
-Pond.Duck.Online.submitDuckForm_ = function(requiredFieldsIds, formId, action) {
-  for(var i = 0, fieldId; (fieldId = requiredFieldsIds[i]); i++) {
-    var el = document.getElementById(fieldId);
-    if (!el.value.trim()) {
-      el.value = '';
-      el.focus();
-      return;
-    }
-  }
-  var form = document.getElementById(formId);
-  var data = Pond.Duck.Online.encodeFormElements_(form);
-  var onLoadCallback = Pond.Duck.Online.createDuckFormOnLoadCallback_(action);
-  BlocklyStorage.makeRequest(form.action, data.join('&'), onLoadCallback);
-  BlocklyDialogs.hideDialog(true);
-};
-/**
- * Create a duck form.
- */
-Pond.Duck.Online.duckCreateForm = function() {
-  Pond.Duck.Online.submitDuckForm_(['duckCreateName'], 'duckCreateForm', 'created');
-};
-/**
- * Update a duck form.
- */
-Pond.Duck.Online.duckUpdateForm = function() {
-  Pond.Duck.Online.submitDuckForm_(['duckUpdateDuckKey'], 'duckUpdateForm', 'updated');
-};
-/**
- * Delete a duck form.
- */
-Pond.Duck.Online.duckDeleteForm = function() {
-  Pond.Duck.Online.submitDuckForm_(['duckDeleteDuckKey'], 'duckDeleteForm', 'deleted');
 };
 
 window.addEventListener('load', Pond.Duck.Online.init);
