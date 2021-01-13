3e54737f88282ec8ffeb634f61d2e3b74f0d3d46
Christopher Allen <cpcallen+git@google.com>
2021-01-13 15:08:20 +0000

    Convert default service from python27 to nodejs12
    - Enable VPC ("virtual private cloud") connector to enable connections
      from GAE to GCE instances.
    
    - Disable /storage, /admin, /gallery-api/* and /*-redit handlers since
      they were backed by "script:" directives (and, in the case of /admin,
      requires "login: admin") which are not supported by newer AppEngine
      environments; all these will be supplanted by new backend.
    
    - Move list of files to be skipped by gcloud deploy to .gcloudignore, as
      "skip_files:" directive is no longer supported.

--- appengine/deploy.sh
+++ appengine/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo 'Old deploy script disabled as a safety precaution.'
+exit 0
+
 # Script to deploy on GAE.
 
 gcloud app deploy app.yaml --project blockly-games --version 3 --no-promote
