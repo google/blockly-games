c55bdd3b0ac242a5e150c2a2aa318bb4d1d81385
Neil Fraser <fraser@google.com>
2021-09-29 12:01:11 -0700

    Fix ok/cancel dialog button labels
    Resolves #226

--- appengine/js/lib-dialogs.js
+++ appengine/js/lib-dialogs.js
@@ -86,11 +86,11 @@ BlocklyDialogs.showDialog = function(content, origin, animate, modal, style,
   // Inject 'Ok' and 'Cancel' from Blockly's messages.
   var okButtons = content.getElementsByClassName('dialogOk');
   for (var i = 0; i < okButtons.length; i++) {
-    okButtons[i].textContent = Blockly.Msg.DIALOG_OK;
+    okButtons[i].textContent = Blockly.Msg['DIALOG_OK'];
   }
   var cancelButtons = content.getElementsByClassName('dialogCancel');
   for (var i = 0; i < cancelButtons.length; i++) {
-    cancelButtons[i].textContent = Blockly.Msg.DIALOG_CANCEL;
+    cancelButtons[i].textContent = Blockly.Msg['DIALOG_CANCEL'];
   }
   dialog.appendChild(content);
   content.className = content.className.replace('dialogHiddenContent', '');
